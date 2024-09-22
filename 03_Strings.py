8c2ae72be6f2f7e4d744722cb42f5bb72fbfb7e4
Ayush Tambe <ayushtambe48@gmail.com>
2024-09-22 18:27:01 +0530

    Update 03_Strings.py

--- 03_Strings.py
+++ 03_Strings.py
@@ -1,75 +1,51 @@
-# strings
+"""
+Chapter 3: Strings in Python
+This script demonstrates basic string operations, slicing, and string functions.
+Author: Ayush Tambe
+Date: September 2024
+"""
 
+# Example of string slicing
 name = "ayush"
-nameshort = name[0:3] # '0' means starting index and '3' means ending index 
-# ^^^ start from index 0 all thr way till 3(excluding 3)
-
-print(nameshort)# this will be print 'ayu'because i givned thh ending index is 3
-
+nameshort = name[:3]  # Slice string from index 0 to 2 (excluding 3)
+print(nameshort)  # Output: 'ayu'
 
 # Negative Slicing
-# in negative slicing index start from ending with -1
-# e.g harry = ( y = -1), (r = -2) ,(r = -3) etc
-print(name[-4:-1])#same as
-print(name[1:4])
+# Demonstrates how slicing from the end works
+print(name[-4:-1])  # Output: 'yus'
+print(name[1:4])    # Output: 'yus'
 
-print(name[:4])# same as print(name[0:4])
-print(name[0:4])
-print(name[1:])# same as print(name[1:5])
-print(name[1:5])
+# Demonstrating string functions
+print(len(name))  # Length of the string
+print(name.endswith("ush"))  # True if name ends with 'ush'
+print(name.startswith("ayu"))  # True if name starts with 'ayu'
 
+# Replacing strings
+new_name = name.replace("ayush", "tambe")
+print(new_name)  # Output: 'tambe'
 
-# STRINGS Function
-# endswith,startswith,capitalize,upper,lower,count,replace
-print(len(name))
-print(name.endswith("ush"))# if name is end with yus the print yes
-print(name.endswith("ayu"))
+# Escape sequence characters
+print("Name is Ayush Nilesh Tambe\nSalary: 20 crore")
 
-print(name.startswith("ayu"))# if name is start with ayu the print yes
-print(name.startswith("ush"))
+# Practice problems
+# Problem 1: Take user input and display a greeting
+a = input("Enter your name: ")
+print(f"Good afternoon, {a}")
 
-# Replace string
-replacestring = name.replace("ayush","tambe")
-print(replacestring)
-
-#Escape sequence characters = \n, \t , ;\ , \ , \'ayush\' ,  etc...
-print("name is ayush nilesh tambe\nsal is 20 core  ")
-
-
-# practice set
-# 1
-
-a = input("enetr your name: ")
-print("good afternoon  ",a)
-print(f"good afternoon {a} ")# if you give the 'f' string
-
-# 2
-
-letter='''Dear <name>,
-you are selected!
+# Problem 2: Use a template letter
+letter = '''Dear <name>,
+You are selected!
 <Date>'''
-print(letter.replace("<name>","ayush").replace("<Date>","01-01-2025"))
-print(f"Dear {a}\nyou are selected!\n 01-01-2025")
-
-# 3
-
-name= "ayush  tambe"
-print(name.find("  "))
-
-# 4
-
-print(name.replace("  "," "))
-
-# 5
-letter = "Dear harry,\nthis python course is nice.\n Thanks"
+letter = letter.replace("<name>", "Ayush").replace("<Date>", "01-01-2025")
 print(letter)
 
+# Problem 3: Find double spaces in a string
+name_with_spaces = "ayush  tambe"
+print(name_with_spaces.find("  "))
 
+# Problem 4: Replace double spaces with single spaces
+print(name_with_spaces.replace("  ", " "))
 
-
-
-
-
-
-
-
+# Problem 5: Format a letter using escape sequences
+formatted_letter = "Dear Harry,\nThis Python course is great.\nThanks"
+print(formatted_letter)
